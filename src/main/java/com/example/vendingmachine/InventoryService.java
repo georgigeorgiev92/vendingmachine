104ebb571954637857c890ab9b5004c5906144d3
georgi <georgi@pop-os.localdomain>
2023-01-12 15:27:12 +0200

    some changes

--- src/main/java/com/example/vendingmachine/InventoryService.java
+++ src/main/java/com/example/vendingmachine/InventoryService.java
@@ -12,12 +12,17 @@ public class InventoryService {
 
     @Autowired
     private ProductRepository productRepository;
+    // check if the inventory is <10
+    // if it is, update the quantity
+    // if not, throw exception
     public Product addProduct(Product product) throws Exception {
         if(product.getName() == null)
             throw new Exception("Some value(s) are missing, Please check the manual properly");
         return productRepository.save(product);
     }
 
+    // find the product in the inventory
+    // update its details
     public Product updateProduct(Product product) throws Exception {
         Product product1 = getProduct(product.getId());
         if(product1 == null)
@@ -31,6 +36,9 @@ public class InventoryService {
             throw new Exception(("No Such Product exist"));
         return product.get();
     }
+
+    // find the product in the inventory
+    // remove it from the inventory
     public boolean removeProduct(Long id) throws Exception {
         Product product = getProduct(id);
         //ProductRepository.delete(product);
